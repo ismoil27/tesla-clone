b39aaf3d01d5d1168364d3604a2834e431fccf44
Ismoiljon (Jon) <abduraimovismoil98@gmail.com>
2022-04-13 14:17:01 +0900

    started making tesla clone

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Section from "../Section";
+import { Container } from "./style";
+
+const Home = () => {
+  return (
+    <Container>
+      <Section />
+    </Container>
+  );
+};
+
+export default Home;
---
+++ src/components/root/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header";
+import Home from "../Home";
+
+const Root = () => {
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
+};
+
+export default Root;
